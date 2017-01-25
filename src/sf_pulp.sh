f7e588b6654a4455f34d89219bbc93c8ee9c9b1c
Francois Laupretre <francois@tekwire.net>
2017-01-25 12:59:33 +0100

    pulp: Support multiple files in pulp_rpm_upload() and pulp_rpm_remove()

--- src/sf_pulp.sh
+++ src/sf_pulp.sh
@@ -8,27 +8,34 @@
 #=============================================================================
 
 ##------------------------------------------------
-# Remove a RPM package
+# Remove RPM package(s)
 #
 # Args:
 #	$1: Repo name (repo-id)
-#	$2: RPM package
+#	$2-*: RPM package(s) to remove
 # Returns: 0 if OK. !=0 if not
 # Displays: Pulp info messages
 #------------------------------------------------
 
 function sf_pulp_rpm_remove
 {
-typeset repo pkg
+typeset repo pkgs pkg ret
+
+ret=0
 
 repo="$1"
 [ "X$repo" = X ] && sf_fatal "No repo specified" && return 1
-pkg="$2"
-[ -z "$pkg" ] && sf_fatal "Empty package name" && return 1
+shift
+pkgs="$*"
+[ -z "$pkgs" ] && sf_fatal "No package specified" && return 1
 
 #---
 
-pulp-admin rpm repo remove rpm --repo-id $repo --str-eq "filename=$pkg"
+for pkg in $pkgs ; do
+	pulp-admin rpm repo remove rpm --repo-id $repo --str-eq "filename=$pkg" || ret=1
+done
+
+return $ret
 }
 
 ##------------------------------------------------
@@ -53,38 +60,45 @@ pulp-admin rpm repo publish run --repo-id $repo
 }
 
 ##------------------------------------------------
-# Upload a RPM package
+# Upload RPM package(s)
 #
 # Args:
 #	$1: Repo name (repo-id)
-#	$2: RPM package
+#	$2-*: RPM packages
 # Returns: 0 if OK. !=0 if not
 # Displays: Pulp info messages
 #------------------------------------------------
 
 function sf_pulp_rpm_upload
 {
-typeset repo pkg
+typeset repo pkgs pkg ret
+
+ret=0
 
 repo="$1"
 [ "X$repo" = X ] && sf_fatal "No repo specified" && return 1
-pkg="$2"
-[ ! -f "$pkg" ] && sf_fatal "$pkg: File not found" && return 1
+shift
+pkgs="$*"
+[ -z "$pkgs" ] && sf_fatal "No package specified" && return 1
 
 #---
 
-#-- First, remove package if it is already existing in the repo
-#-- Known bug (see https://bugzilla.redhat.com/show_bug.cgi?id=1041317)
+for pkg in $pkgs ; do
+	#-- First, remove package if it is already existing in the repo
+	#-- Known bug (see https://bugzilla.redhat.com/show_bug.cgi?id=1041317)
 
-sf_pulp_rpm_remove $repo $pkg || :
+	sf_pulp_rpm_remove $repo $pkg || :
 
-#-- Upload
+	#-- Upload
 
-pulp-admin rpm repo uploads rpm --file "$pkg" --repo-id  "$repo" || return 1
+	pulp-admin rpm repo uploads rpm --file "$pkg" --repo-id  "$repo" || ret=1
+done
 
 # Auto-publish does not seem to work...
 
 sf_pulp_rpm_publish $repo
+
+return $ret
 }
 
 #=============================================================================
